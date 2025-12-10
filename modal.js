766755814ff8d60d60bf8f9d7ec741e36f38f1ba
Olha Fursova <teor15manypeople1@gmail.com>
2025-12-11 00:48:43 +0100

    Fix dropdown closing when the mobile menu is closed

--- modal.js
+++ modal.js
@@ -3,19 +3,33 @@ const closeBtn = document.querySelector(".js-close-menu");
 const modalOverlay = document.querySelector(".modal-overlay");
 const menuLinks = modalOverlay.querySelectorAll("a");
 
+const dropdowns = modalOverlay.querySelectorAll(
+  ".features-mobile, .company-mobile"
+);
+const dropdownIcons = modalOverlay.querySelectorAll(
+  ".features-dropdown-mobile use, .company-dropdown-mobile use"
+);
+
 openBtn.addEventListener("click", () => {
   modalOverlay.classList.add("is-open");
   document.body.classList.add("modal-open");
 });
 
-closeBtn.addEventListener("click", () => {
+const closeMenu = () => {
   modalOverlay.classList.remove("is-open");
   document.body.classList.remove("modal-open");
-});
+
+  dropdowns.forEach((dropdown) => {
+    dropdown.classList.remove("active");
+  });
+
+  dropdownIcons.forEach((icon) => {
+    icon.setAttribute("href", "./img/icons.svg#icon-path-closed");
+  });
+};
+
+closeBtn.addEventListener("click", closeMenu);
 
 menuLinks.forEach((link) => {
-  link.addEventListener("click", () => {
-    modalOverlay.classList.remove("is-open");
-    document.body.classList.remove("modal-open");
-  });
+  link.addEventListener("click", closeMenu);
 });
